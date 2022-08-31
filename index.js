3209c61eaa7600fed1c544e5cdf8d84d176bbaa6
Kiwg <gkiwure21@gmail.com>
2022-08-31 15:14:07 +0300

    Done

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function addingEventListener()
     const buttonClick = document.getElementById('button');
 
     function clickAlert() {
-        alert('Button has been c
+        alert('Button has been clicked')
     }
